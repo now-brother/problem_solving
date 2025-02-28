b5e739f7f677f0bd3a80882230d35d83b2943b77
now-brother <nomagold57@gmail.com>
2025-02-28 19:23:44 +0900

    ADD : 2025-02-28

---
+++ Java/programmers/최소직사각형/Solution.java
@@ -0,0 +1,31 @@
+package Java.programmers.최소직사각형;
+
+public class Solution {
+    public int solution(int[][] sizes) {
+        int answerS = 0;
+        int answerL = 0;
+        int testS = 0;
+        int testL = 0;
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] <= sizes[i][1]){
+                testS = sizes[i][0];
+                testL = sizes[i][1];
+            }else{
+                testS = sizes[i][1];
+                testL = sizes[i][0];
+            }
+            if(i == 0){
+                answerS = testS;
+                answerL = testL;
+            }else{
+                if(testS > answerS){
+                    answerS = testS;
+                }
+                if(testL > answerL) {
+                    answerL = testL;
+                }
+            }
+        }
+        return answerS * answerL;
+    }
+}
