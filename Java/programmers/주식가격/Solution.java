09fbc48e3141cb95ed705d273461981da3973f56
now-brother <nomagold57@gmail.com>
2025-03-05 13:14:10 +0900

    ADD : 2025-03-05

---
+++ Java/programmers/주식가격/Solution.java
@@ -0,0 +1,30 @@
+package Java.programmers.주식가격;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < n; i++){
+            int cnt = 0;
+            for(int j = i + 1; j < n; j++){
+                if(prices[i] > prices[j]){
+                    cnt += 1;
+                    break;
+                }else{
+                    cnt += 1;
+                }
+            }
+            stack.push(cnt);
+        }
+        
+        for(int i = 0; i < n; i++){
+            answer[n - i - 1] = stack.pop();
+        }
+        return answer;
+    }
+}
