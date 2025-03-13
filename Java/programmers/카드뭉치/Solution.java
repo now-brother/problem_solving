8bd00c8683019915e1084b34d2c604cf1f126be3
now-brother <nomagold57@gmail.com>
2025-03-13 18:33:48 +0900

    ADD : 2025-03-13

---
+++ Java/programmers/카드뭉치/Solution.java
@@ -0,0 +1,27 @@
+package Java.programmers.카드뭉치;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> queueCard1 = new ArrayDeque<>(Arrays.asList(cards1));
+        Queue<String> queueCard2 = new ArrayDeque<>(Arrays.asList(cards2));
+        Queue<String> queueGoal = new ArrayDeque<>(Arrays.asList(goal));
+        
+        String answer = "Yes";
+        while(!queueGoal.isEmpty()){
+            if(!queueCard1.isEmpty() && queueCard1.peek().equals(queueGoal.peek())){
+                queueCard1.poll();
+                queueGoal.poll();
+            }else if(!queueCard2.isEmpty() && queueCard2.peek().equals(queueGoal.peek())){
+                
+                queueCard2.poll();
+                queueGoal.poll();
+            }else{
+                answer = "No";
+                break;
+            }
+        }
+        return answer;
+    }
+}
