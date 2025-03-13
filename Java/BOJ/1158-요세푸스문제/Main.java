1f40daaf0c06e7998f005553e69edfd0eb693dd3
now-brother <nomagold57@gmail.com>
2025-03-13 17:00:14 +0900

    ADD : 2025-03-13

---
+++ Java/BOJ/1158-요세푸스문제/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        for(int i = 1; i < N + 1; i++){
+            deque.addLast(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(deque.size() > 1){
+            for(int i = 0; i < K - 1; i++){
+                deque.addLast(deque.pollFirst());
+            }
+            sb.append(deque.pollFirst() + ", ");
+        }
+
+        sb.append(deque.pollFirst() + ">");
+
+        bw.write(sb.toString() + "\n");
+        bw.flush();
+
+    }
+}
