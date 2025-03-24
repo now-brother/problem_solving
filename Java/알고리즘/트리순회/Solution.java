1d40ba3ef463a787dc0c40aaf9338e63f2c2851b
now-brother <nomagold57@gmail.com>
2025-03-24 19:24:52 +0900

    ADD : 2025-03-24

---
+++ Java/알고리즘/트리순회/Solution.java
@@ -0,0 +1,40 @@
+package Java.알고리즘.트리순회;
+
+public class Solution {
+    public static String[] solution(int[] nodes){
+        String[] result = new String[3];
+        result[0] = preorder(nodes, 0).trim();
+        result[1] = inorder(nodes, 0).trim();
+        result[2] = postorder(nodes, 0).trim();
+
+        return result;
+    }
+
+    private static String preorder(int[] nodes, int idx){
+        if(idx >= nodes.length)
+            return "";
+
+        return nodes[idx] + " " +
+                preorder(nodes, idx * 2 + 1) +
+                preorder(nodes, idx * 2 + 2);
+    }
+
+
+    private static String inorder(int[] nodes, int idx){
+        if(idx >= nodes.length)
+            return "";
+
+        return inorder(nodes, idx * 2 + 1) +
+                nodes[idx] + " " +
+                inorder(nodes, idx * 2 + 2);
+    }
+
+    private static String postorder(int[] nodes, int idx){
+        if(idx >= nodes.length)
+            return "";
+
+        return postorder(nodes, idx * 2 + 1) +
+                postorder(nodes, idx * 2 + 2) +        
+                nodes[idx] + " ";
+    }
+}
