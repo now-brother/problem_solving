193e22035e296533d4f064ce868b4101c60adbd1
now-brother <nomagold57@gmail.com>
2025-03-29 16:28:27 +0900

    ADD : 2025-03-29

---
+++ Java/programmers/추억점수/Solution.java
@@ -0,0 +1,26 @@
+package Java.programmers.추억점수;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> hash = new HashMap<>();
+        
+        for(int i = 0; i < name.length; i++){
+            hash.put(name[i], yearning[i]);
+        }
+        
+        for(int i = 0; i < photo.length; i++){
+            String[] photos = photo[i];
+            int score = 0;
+            for(int j = 0; j < photos.length; j++){
+                score = score + hash.getOrDefault(photos[j], 0);
+            }
+            answer[i] = score;
+        }
+        
+        return answer;
+    }
+}
