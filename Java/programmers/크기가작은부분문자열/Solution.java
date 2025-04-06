5c7620fbe1c2e16014b88b6d40a6dc868a030c50
now-brother <nomagold57@gmail.com>
2025-04-06 15:00:40 +0900

    ADD : 2025-04-06

---
+++ Java/programmers/크기가작은부분문자열/Solution.java
@@ -0,0 +1,21 @@
+package Java.programmers.크기가작은부분문자열;
+
+public class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        
+        int lent = t.length();
+        int lenp = p.length();
+        long intp = Long.parseLong(p);
+
+        for(int i = 0; i < lent - lenp + 1; i++){
+            String test = t.substring(i, i+lenp);
+            long intt = Long.parseLong(test);
+            if(intt <= intp)
+                answer++;
+        }
+        
+        return answer;
+    }
+}
+    
