1521a6ee9d08492eb3770ddb15992cbe1392c459
now-brother <nomagold57@gmail.com>
2025-04-11 09:57:47 +0900

    ADD : 2025-04-11

---
+++ Java/programmers/가장가까운글자/Solution.java
@@ -0,0 +1,24 @@
+package Java.programmers.가장가까운글자;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int n = s.length();    
+        int[] answer = new int[n];
+        HashMap<Character, Integer> hash = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            char c = s.charAt(i);
+            if(hash.containsKey(c)){
+                answer[i] = i - hash.get(c);
+                hash.put(c, i);
+            }else{
+                hash.put(c, i);
+                answer[i] = -1;
+            }
+        }
+        
+        return answer;
+    }
+}
