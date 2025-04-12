db187419453e2c192afb92a4b29684d8eaf222e9
now-brother <nomagold57@gmail.com>
2025-04-13 08:10:18 +0900

    ADD : 2025-04-13

---
+++ Java/programmers/석유시추/Solution.java
@@ -0,0 +1,64 @@
+package Java.programmers.석유시추;
+
+import java.util.*;
+
+class Solution {
+
+    private static int[][] lands;
+    private static boolean[][] check;
+    private static int cnt;
+    private static int n;
+    private static int m;
+    private static int[] dx = {0, 0, -1, 1};
+    private static int[] dy = {-1, 1, 0, 0};
+    private static Set<Integer> columnSet;
+
+    public static void dfs(int x, int y) {
+        check[y][x] = true;
+        cnt++;
+        if (x >= 0 && x < m) {
+            columnSet.add(x);
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                if (lands[ny][nx] == 1 && !check[ny][nx]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+
+    public int solution(int[][] land) {
+        int answer = 0;
+        n = land.length;
+        m = land[0].length;
+        lands = land;
+        check = new boolean[n][m];
+
+        int[] colOil = new int[m];
+
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < m; x++) {
+                if (lands[y][x] == 1 && !check[y][x]) {
+                    cnt = 0;
+                    columnSet = new HashSet<>();
+                    dfs(x, y);
+
+                    for (int col : columnSet) {
+                        if (col >= 0 && col < m) {
+                            colOil[col] += cnt;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int oil : colOil) {
+            answer = Math.max(answer, oil);
+        }
+
+        return answer;
+    }
+}
