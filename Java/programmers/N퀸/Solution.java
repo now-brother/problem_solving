60b56f538312210add690fd8bcb1e9d3a91e7edc
now-brother <nomagold57@gmail.com>
2025-04-15 11:20:42 +0900

    ADD : 2025-04-15

---
+++ Java/programmers/N퀸/Solution.java
@@ -0,0 +1,34 @@
+package Java.programmers.N퀸;
+
+class Solution {
+    private int N = 0;
+
+    public int solution(int n) {
+        int[] queens = new int[n];
+        placeQueen(0, queens, n);
+        return N;
+    }
+
+    private void placeQueen(int row, int[] queens, int n) {
+        if (row == n) {
+            N++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, queens)) {
+                queens[row] = col;
+                placeQueen(row + 1, queens, n);
+            }
+        }
+    }
+
+    private boolean isValid(int row, int col, int[] queens) {
+        for (int i = 0; i < row; i++) {
+            if (queens[i] == col || Math.abs(row - i) == Math.abs(col - queens[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
