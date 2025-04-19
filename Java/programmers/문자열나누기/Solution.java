0facc83dc10c7337b2d40e840d0a6d4f43c4decb
now-brother <nomagold57@gmail.com>
2025-04-19 20:07:40 +0900

    2025-04-19

---
+++ Java/programmers/문자열나누기/Solution.java
@@ -0,0 +1,29 @@
+package Java.programmers.문자열나누기;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int same = 0, diff = 0;
+        char first = s.charAt(0);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (same == 0) {
+                first = s.charAt(i);
+            }
+
+            if (s.charAt(i) == first) same++;
+            else diff++;
+
+            if (same == diff) {
+                answer++;
+                same = 0;
+                diff = 0;
+            }
+        }
+
+        if (same != 0 || diff != 0) answer++;
+
+        return answer;
+    }
+}
+
