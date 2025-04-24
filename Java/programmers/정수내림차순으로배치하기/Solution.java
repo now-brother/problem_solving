ec4d8b89f18829df5cc6bf3de54c570e3e255c8d
now-brother <nomagold57@gmail.com>
2025-04-24 13:26:05 +0900

    ADD : 2025-04-24

---
+++ Java/programmers/정수내림차순으로배치하기/Solution.java
@@ -0,0 +1,21 @@
+package Java.programmers.정수내림차순으로배치하기;
+
+class Solution {
+    public long solution(long n) {
+        String ns = String.valueOf(n);
+        char[] arr = ns.toCharArray();
+        for(int i = 0; i < ns.length(); i++){
+            for(int j = i; j < ns.length(); j++){
+                if(arr[i] < arr[j]){
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        String result = new String(arr);
+        long answer = Long.parseLong(result);
+        
+        return answer;
+    }
+}
