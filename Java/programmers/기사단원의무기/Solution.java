057ca12cdb70c0a185999f224f2fb27a7c4a5b03
now-brother <nomagold57@gmail.com>
2025-04-26 11:06:42 +0900

    ADD : 2025-04-26

---
+++ Java/programmers/기사단원의무기/Solution.java
@@ -0,0 +1,23 @@
+package Java.programmers.기사단원의무기;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int result = 0;
+        
+        for (int i = 1; i <= number; i++) {
+            int cnt = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    cnt++;
+                    if (j != i / j) cnt++;
+                }
+            }
+            if (cnt > limit)
+                result += power;
+            else
+                result += cnt;
+        }
+        
+        return result;
+    }
+}
