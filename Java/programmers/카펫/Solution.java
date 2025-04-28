3b8e534b6a8d9faa9d31937f2413919a4356fb32
now-brother <nomagold57@gmail.com>
2025-04-28 10:58:15 +0900

    ADD : 2025-04-28

---
+++ Java/programmers/카펫/Solution.java
@@ -0,0 +1,22 @@
+package Java.programmers.카펫;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int n = brown + yellow;
+        
+        for(int i = 1; i <= n/2; i++){
+            if(n % i == 0){
+                int w = i;
+                int h = n / i;
+                if((w - 2) * (h - 2) == yellow){
+                    answer[0] = h;
+                    answer[1] = w;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
