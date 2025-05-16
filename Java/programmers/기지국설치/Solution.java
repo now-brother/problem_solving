33ff0406acfc1c64a5b5dd24c0f5c76a7d9cefe7
now-brother <nomagold57@gmail.com>
2025-05-16 17:44:09 +0900

    ADD : 2025-05-16

---
+++ Java/programmers/기지국설치/Solution.java
@@ -0,0 +1,23 @@
+package Java.programmers.기지국설치;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int coverage = 2 * w + 1; 
+        int position = 1; 
+        int index = 0; 
+
+        while (position <= n) {
+            if (index < stations.length && position >= stations[index] - w) {
+                position = stations[index] + w + 1;
+                index++;
+            } else {
+                answer++;
+                position += coverage;
+            }
+        }
+        return answer;
+    }
+}
