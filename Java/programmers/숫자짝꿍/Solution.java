dc49cc0dd0ffaff63dbcf5b85448c840bdf90754
now-brother <nomagold57@gmail.com>
2025-05-17 11:01:44 +0900

    ADD : 2025-05-17

---
+++ Java/programmers/숫자짝꿍/Solution.java
@@ -0,0 +1,26 @@
+package Java.programmers.숫자짝꿍;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] xCount = new int[10];
+        int[] yCount = new int[10];
+
+        for (char c : X.toCharArray()) {
+            xCount[c - '0']++;
+        }
+        for (char c : Y.toCharArray()) {
+            yCount[c - '0']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            int common = Math.min(xCount[i], yCount[i]);
+            for (int j = 0; j < common; j++) {
+                sb.append(i);
+            }
+        }
+        if (sb.length() == 0) return "-1";        
+        if (sb.charAt(0) == '0') return "0";      
+        return sb.toString();                     
+    }
+}
