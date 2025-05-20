5970ccc3f991be41b460fa2b788adc75aa074114
now-brother <nomagold57@gmail.com>
2025-05-20 10:38:48 +0900

    ADD : 2025-05-20

---
+++ Java/programmers/숫자문자열과영단어/Solution.java
@@ -0,0 +1,50 @@
+package Java.programmers.숫자문자열과영단어;
+
+class Solution {
+    public int solution(String s) {
+        String answer = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                answer += s.charAt(i);
+            }else if(s.charAt(i) == 'z'){
+                answer += '0';
+                i += 3;
+            }else if(s.charAt(i) == 'o'){
+                answer += '1';
+                i += 2;
+            }else if(s.charAt(i) == 't'){
+                if(s.charAt(i + 1) == 'w'){
+                    answer += '2';
+                    i += 2;
+                }else{
+                    answer += '3';
+                    i += 4;
+                }
+            }else if(s.charAt(i) == 'f'){
+                if(s.charAt(i + 1) == 'o'){
+                    answer += '4';
+                }else{
+                    answer += '5';
+                }
+                i += 3;
+            }else if(s.charAt(i) == 's'){
+                if(s.charAt(i + 1) == 'i'){
+                    answer += '6';
+                    i += 2;
+                }else{
+                    answer += '7';
+                    i += 4;
+                }
+            }else if(s.charAt(i) == 'e'){
+                answer += '8';
+                i += 4;
+            }else if(s.charAt(i) == 'n'){
+                answer += '9';
+                i += 3;
+            }
+        }
+        
+        return Integer.parseInt(answer);
+    }
+}
