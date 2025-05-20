10529ae4780c13cc13298d54755db569f37c833c
now-brother <nomagold57@gmail.com>
2025-05-21 08:42:00 +0900

    ADD : 2025-05-21

---
+++ Java/programmers/약수의개수와덧셈/Solution.java
@@ -0,0 +1,22 @@
+package Java.programmers.약수의개수와덧셈;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i = left; i < right + 1; i++){
+            int cnt = 0;
+            for(int j = 1; j < i + 1; j++){
+                if(i % j == 0)
+                    cnt++;
+            }
+            if(cnt % 2 == 0){
+                answer += i;
+            }else{
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+}
