095e5e03e211e2e720a891aa158c498b5807fb19
now-brother <nomagold57@gmail.com>
2025-05-27 10:42:07 +0900

    ADD : 2025-05-27

---
+++ Java/programmers/체육복/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        Set<Integer> reserveSet = new HashSet<>();
+        Set<Integer> lostSet = new HashSet<>();
+
+        for (int r : reserve) reserveSet.add(r);
+        for (int l : lost) {
+            if (reserveSet.contains(l)) {
+                reserveSet.remove(l);
+            } else {
+                lostSet.add(l);
+            }
+        }
+
+        for (int l : lostSet) {
+            if (reserveSet.contains(l - 1)) {
+                reserveSet.remove(l - 1);
+            } else if (reserveSet.contains(l + 1)) {
+                reserveSet.remove(l + 1);
+            } else {
+                answer--; 
+            }
+        }
+
+        return answer;
+    }
+}
