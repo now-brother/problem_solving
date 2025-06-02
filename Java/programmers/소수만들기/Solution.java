e59c5c6206696584169d4b47f0bf5d57e7fbe09e
now-brother <nomagold57@gmail.com>
2025-06-02 10:44:44 +0900

    ADD : 2025-06-02

---
+++ Java/programmers/소수만들기/Solution.java
@@ -0,0 +1,27 @@
+package Java.programmers.소수만들기;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    int temp = nums[i] + nums[j] + nums[k];
+                    if(isPrime(temp))
+                        answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    private boolean isPrime(int x){
+        if (x <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+           if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
