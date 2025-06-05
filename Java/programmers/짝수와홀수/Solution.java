a77f065239419be0610fa5bafe2df144c30b0b93
now-brother <nomagold57@gmail.com>
2025-06-05 13:00:19 +0900

    ADD : 2025-06-05

---
+++ Java/programmers/짝수와홀수/Solution.java
@@ -0,0 +1,13 @@
+package Java.programmers.짝수와홀수;
+
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
