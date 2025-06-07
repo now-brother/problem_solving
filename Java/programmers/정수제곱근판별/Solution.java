5d785032a5bf06a4a0a129276659b91a9c3552c4
now-brother <nomagold57@gmail.com>
2025-06-07 17:56:41 +0900

    ADD : 2025-06-07

---
+++ Java/programmers/정수제곱근판별/Solution.java
@@ -0,0 +1,25 @@
+package Java.programmers.정수제곱근판별;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 1;
+        
+        while(true){
+            long temp = answer * answer;
+            
+            if(temp == n){
+                answer = (answer + 1) * (answer + 1);
+                break;
+            }
+            
+            if(temp > n){
+                answer = -1;
+                break;
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
