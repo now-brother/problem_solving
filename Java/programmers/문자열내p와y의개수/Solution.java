98d167e1c2dcc0e396f5bb2a4bc4e80f3c7b9d8b
now-brother <nomagold57@gmail.com>
2025-06-19 22:19:45 +0900

    ADD : 2025-06-19

---
+++ Java/programmers/문자열내p와y의개수/Solution.java
@@ -0,0 +1,20 @@
+package Java.programmers.문자열내p와y의개수;
+
+class Solution {
+    boolean solution(String s) {
+        int countp = 0;
+        int county = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P'){
+                countp++;
+            }else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y'){
+                county++;
+            }
+        }
+        
+        if(countp == county)
+            return true;
+        return false;
+    }
+}
