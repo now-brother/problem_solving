da87db731abf5175406bd4372d89658bfa1952b3
now-brother <nomagold57@gmail.com>
2025-06-19 21:54:53 +0900

    ADD : 2025-06-19

---
+++ Java/programmers/소수찾기/Solution.java
@@ -0,0 +1,19 @@
+package Java.programmers.소수찾기;
+
+class Solution {
+    public int solution(int n) {
+        boolean[] isNotPrime = new boolean[n + 1];
+        int count = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (!isNotPrime[i]) {
+                count++;
+                for (int j = i * 2; j <= n; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+}
