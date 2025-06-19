aefc8aef313ef9615a5f36f1797c848423353855
now-brother <nomagold57@gmail.com>
2025-06-19 21:49:31 +0900

    ADD : 2025-06-19

---
+++ Java/programmers/수박수박수박수박수박수/Solution.java
@@ -0,0 +1,17 @@
+package Java.programmers.수박수박수박수박수박수;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                answer += "수";
+            }else{
+                answer += "박";
+            }
+        }
+        
+        return answer;
+    }
+}
