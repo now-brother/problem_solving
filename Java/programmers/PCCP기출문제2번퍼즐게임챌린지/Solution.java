d0d38dc97d30d3b68a211d231269356cb071faf8
now-brother <nomagold57@gmail.com>
2025-09-22 09:32:25 +0900

    ADD : 2025-09-22

--- Java/programmers/PCCP기출문제2번퍼즐게임챌린지/Solution.java
+++ Java/programmers/PCCP기출문제2번퍼즐게임챌린지/Solution.java
@@ -2,26 +2,38 @@ package Java.programmers.PCCP기출문제2번퍼즐게임챌린지;
 
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int level = 1;
+        int left = 1;
+        int right = 1_000_000;
+        int answer = right;
 
-        while (true) {
-            long total = 0;
-            boolean possible = true;
+        while (left <= right) {
+            int mid = (left + right) / 2;
 
-            for (int i = 0; i < times.length; i++) {
-                int wrong = Math.max(0, diffs[i] - level);
-                int prev = (i == 0 ? 0 : times[i - 1]);
-
-                total += (long) wrong * (prev + times[i]) + times[i];
-
-                if (total > limit) {
-                    possible = false;
-                    break;
-                }
+            if (canClear(diffs, times, limit, mid)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-
-            if (possible) return level;
-            level++;
         }
+
+        return answer;
+    }
+
+    private boolean canClear(int[] diffs, int[] times, long limit, int level) {
+        long total = 0;
+
+        for (int i = 0; i < times.length; i++) {
+            int wrong = Math.max(0, diffs[i] - level);
+            int prev = (i == 0 ? 0 : times[i - 1]);
+
+            total += (long) wrong * (prev + times[i]) + times[i];
+
+            if (total > limit) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
