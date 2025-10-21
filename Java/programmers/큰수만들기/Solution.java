6c200c0dc4d596c97626973aea4c6bea097b3152
now-brother <nomagold57@gmail.com>
2025-10-21 09:51:49 +0900

    ADD : 2025-10-21

---
+++ Java/programmers/큰수만들기/Solution.java
@@ -0,0 +1,27 @@
+package Java.programmers.큰수만들기;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : number.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peekLast() < c) {
+                stack.pollLast();
+                k--;
+            }
+            stack.addLast(c);
+        }
+        while (k-- > 0) stack.pollLast();
+        
+        String answer = "";
+        
+        for(char c : stack){
+            answer += c;
+        }
+
+        return answer;
+    }
+}
