689732e276d04204be55558bab300cd163d5e221
now-brother <nomagold57@gmail.com>
2026-02-01 09:51:47 +0900

    ADD : 2026-02-01

---
+++ Java/programmers/문자열붙여서출력하기/Solution.java
@@ -0,0 +1,13 @@
+package java.programmers.문자열붙여서출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.println(a + "" + b);
+    }
+}
