3f9649162f8fe0c2f7478308e12e83b641fa493f
now-brother <nomagold57@gmail.com>
2026-02-03 09:17:29 +0900

    ADD : 2026-02-03

---
+++ Java/programmers/연속된부분수열의합/Solution.java
@@ -0,0 +1,33 @@
+package java.programmers.연속된부분수열의합;
+
+public class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int n = sequence.length;
+
+        int left = 0;
+        long sum = 0;
+
+        int bestL = 0;
+        int bestR = n - 1;
+        int bestLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < n; right++) {
+            sum += sequence[right];
+
+            while (sum > k && left <= right) {
+                sum -= sequence[left++];
+            }
+
+            if (sum == k) {
+                int len = right - left + 1;
+                if (len < bestLen || (len == bestLen && left < bestL)) {
+                    bestLen = len;
+                    bestL = left;
+                    bestR = right;
+                }
+            }
+        }
+
+        return new int[]{bestL, bestR};
+    }
+}
