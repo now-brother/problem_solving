f972d6b556c6eee7b41ea171a369fc632cbf36c4
now-brother <nomagold57@gmail.com>
2026-02-09 16:23:30 +0900

    ADD : 2026-02-09

---
+++ Java/programmers/고득점알고리즘/해시/완주하지못한선수/Solution.java
@@ -0,0 +1,26 @@
+package java.programmers.고득점알고리즘.해시.완주하지못한선수;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {        
+        String answer = "";
+        
+        HashMap<String, Integer> hash = new HashMap<>();
+        
+        for(String s : completion){
+            hash.put(s, hash.getOrDefault(s,0) + 1);
+        }
+
+        for(String s : participant){
+            if(hash.getOrDefault(s,0) - 1 < 0){
+                answer = s;
+                break;
+            }else{
+                hash.put(s, hash.get(s) - 1);
+            }
+        }
+        
+        return answer;
+    }
+}
