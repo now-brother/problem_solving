c1ed80a590278f2d2373a3ac5de06b2fd5ed342b
now-brother <nomagold57@gmail.com>
2026-02-09 16:25:44 +0900

    Modify : 2026-02-09

--- Java/programmers/문자열붙여서출력하기/Solution.java
+++ Java/programmers/문자열붙여서출력하기/Solution.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
-        
-        System.out.println(a + "" + b);
+        try (Scanner sc = new Scanner(System.in)) {
+            String a = sc.next();
+            String b = sc.next();
+            
+            System.out.println(a + "" + b);
+        }
     }
 }
